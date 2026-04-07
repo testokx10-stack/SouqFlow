9c6844c35de427ee6d719725584f53b1b88eeb07
testokx10-stack <testokx10@gmail.com>
2026-04-07 22:30:45 +0100

    Remove dark overlay from hero background

--- src/App.tsx
+++ src/App.tsx
@@ -166,7 +166,6 @@ function App() {
             backgroundRepeat: 'no-repeat',
           }}
         >
-          <div className="absolute inset-0 bg-black bg-opacity-50"></div>
         </section>
       )}
 
